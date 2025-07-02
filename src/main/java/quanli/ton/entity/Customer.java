13b0f6147453116ac30b38b4bbb9c4d79cd9c397
DevHieu <hieudd2090@gmail.com>
2025-07-02 16:27:37 +0700

    #6-hoan thanh gan het (chua lam khi nhan vao tblBills se mo cua so chi tiet

---
+++ src/main/java/quanli/ton/entity/Customer.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quanli.ton.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author hieud
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Customer {
+    private long id;
+    private String fullName;
+    private String sdt;
+    private String diaChi;
+}
