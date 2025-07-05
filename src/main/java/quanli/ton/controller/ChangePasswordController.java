ef70ee036663cdb214660fda3b9b1000c6bfb6ab
backend <huynhhieu210106@gmail.com>
2025-07-05 10:40:39 +0700

    Products thay đổi

---
+++ src/main/java/quanli/ton/controller/ChangePasswordController.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quanli.ton.controller;
+
+/**
+ *
+ * @author huynhtrunghieu
+ */
+public  interface ChangePasswordController {
+    void open();
+    void save();
+    void close();
+}
